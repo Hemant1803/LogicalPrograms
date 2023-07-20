a3b5379b4a4d9b0a5bf42c3ac031ec3d6124c28a
Hemant1803 <66568416+Hemant1803@users.noreply.github.com>
2023-07-20 08:27:09 +0530

    Add files via upload
    Fibonacci Series

---
+++ FibonacciSeries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        public void Fibonacci()
+        {
+            Console.WriteLine("Enter The First Number :- ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter The Second Number :-");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the no. of Elements to print ");
+            int numofelements = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i < numofelements; i++)
+            {
+                int num3 = num1 + num2;
+                Console.Write(num3+" ");
+                num1 = num2;
+                
+                num2 = num3;
+               
+            }
+
+
+        }
+    }
+}
