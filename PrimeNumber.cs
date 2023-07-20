9a1e6ded454b277df1719d2d3568cf0ab8cf9efe
Hemant1803 <66568416+Hemant1803@users.noreply.github.com>
2023-07-20 08:31:28 +0530

    Add files via upload
    Prime Number

---
+++ PrimeNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PrimeNumber
+    {
+        public void Primenumber() 
+        {
+           int flag = 0;
+            Console.WriteLine("enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 2; i <= num / 2; ++i)
+            {
+                if (num % i == 0)
+                {
+                    flag = 1;
+                    break;
+
+                }
+              
+            }
+            if (flag == 1)
+            {
+                Console.WriteLine("not prime"+num);
+            }
+            else
+            {
+                Console.WriteLine("number is prme" + num);
+            }
+
+
+
+
+        }
+    }
+}
