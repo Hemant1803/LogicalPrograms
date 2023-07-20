f76ac0863adc70182543ab12ad54f4a3a598b2cb
Hemant1803 <66568416+Hemant1803@users.noreply.github.com>
2023-07-20 08:31:00 +0530

    Add files via upload
    Perfect Number

---
+++ PerfectNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PerfectNumber
+    {
+        public void Perfectnumber() {
+            Console.WriteLine("enter a number");
+            int num =Convert.ToInt32(Console.ReadLine());
+            int sum = 1;
+            
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                     sum = sum + i;
+                    Console.Write(i + "," );
+                }
+            }
+            if (sum == num)
+            {
+                Console.WriteLine("\n Entered number is a perfect number");
+               
+            }
+            else
+            {
+                Console.WriteLine("\n Entered number is not a perfect number");
+               
+            }
+
+        }
+    }
+}
