10feced0ac1d405f58f5d6a5e180223c667b66a8
Hemant1803 <66568416+Hemant1803@users.noreply.github.com>
2023-07-20 08:24:54 +0530

    Add files via upload
    Logical Programs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           // FibonacciSeries fibonacciSeries = new FibonacciSeries();
+            //fibonacciSeries.Fibonacci();
+
+           // PerfectNumber perfectNumber = new PerfectNumber();
+            //perfectNumber.Perfectnumber();
+
+           // PrimeNumber primeNumber = new PrimeNumber();
+           // primeNumber.Primenumber();
+
+           // ReverseNumber reverseNumber = new ReverseNumber();
+            //reverseNumber.Reversenumber();
+            CouponNumber couponNumber = new CouponNumber();
+            couponNumber.Couponnumber();
+        }
+    }
+}
