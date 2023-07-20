0c868390913b545b98898bf01bac14c8ea3b3385
Hemant1803 <66568416+Hemant1803@users.noreply.github.com>
2023-07-20 08:32:03 +0530

    Add files via upload
    Reverse Number

---
+++ ReverseNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class ReverseNumber
+    {
+        public void Reversenumber()
+        {
+            int s = 0;
+            Console.WriteLine("numnber");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            while (num !=0)
+            {
+               int r = num % 10;               
+               s = s * 10 + r;
+                num = num / 10;
+                //Console.WriteLine(s);
+                Console.WriteLine(r);
+               // Console.WriteLine(n);
+
+            }
+            Console.WriteLine(s);
+        }
+    }
+}
